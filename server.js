66c7b91d1e8cd0b8c1063088817214a236a66f2d
Enrique Jimenez <ejimenez2292@gmail.com>
2022-04-16 18:54:15 -0500

    added html routes

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+
+//app.use middleware
 app.use(express.urlencoded({ extended: true}))
 
 app.use(express.json())
 
 app.use(express.static('public'))
+
+
+// html routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
 
 
 app.listen(PORT, () => {
